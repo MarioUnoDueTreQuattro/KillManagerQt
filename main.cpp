1773997617865d8bb31a549a695fd172149f90e2
MarioUnoDueTreQuattro <andreagi0@yahoo.it>
2025-09-07 18:50:39 +0200

    style

--- main.cpp
+++ main.cpp
@@ -110,21 +110,21 @@ int main(int argc, char* argv[])
         qDebug() << "Setting Windows style";
     }
     // Create a dark palette
-    QPalette darkPalette;
-    darkPalette.setColor(QPalette::Window, QColor(53, 53, 53));
-    darkPalette.setColor(QPalette::WindowText, Qt::white);
-    darkPalette.setColor(QPalette::Base, QColor(25, 25, 25));
-    darkPalette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
-    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
-    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
-    darkPalette.setColor(QPalette::Text, Qt::white);
-    darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
-    darkPalette.setColor(QPalette::ButtonText, Qt::white);
-    darkPalette.setColor(QPalette::BrightText, Qt::red);
-    darkPalette.setColor(QPalette::Highlight, QColor(142, 45, 197).lighter());
-    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
-    // Apply palette
-    a.setPalette(darkPalette);
+//    QPalette darkPalette;
+//    darkPalette.setColor(QPalette::Window, QColor(53, 53, 53));
+//    darkPalette.setColor(QPalette::WindowText, Qt::white);
+//    darkPalette.setColor(QPalette::Base, QColor(25, 25, 25));
+//    darkPalette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
+//    darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+//    darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+//    darkPalette.setColor(QPalette::Text, Qt::white);
+//    darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
+//    darkPalette.setColor(QPalette::ButtonText, Qt::white);
+//    darkPalette.setColor(QPalette::BrightText, Qt::red);
+//    darkPalette.setColor(QPalette::Highlight, QColor(142, 45, 197).lighter());
+//    darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+//    // Apply palette
+//    a.setPalette(darkPalette);
     QSettings settings;
     bool bLogToFile = settings.value("Dialog/UseLogFile", false).toBool ();
     if (bLogToFile)
